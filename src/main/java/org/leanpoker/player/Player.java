8477ee4a86ecfb3f9e5aec16fa8a6769896aee21
srdjanSrdjan9 <mladjenovic031@gmail.com>
2015-06-27 11:52:21 +0200

    srki
    asd

--- src/main/java/org/leanpoker/player/Player.java
+++ src/main/java/org/leanpoker/player/Player.java
@@ -1,14 +1,18 @@
 package org.leanpoker.player;
 
 import com.google.gson.JsonElement;
+import com.google.gson.JsonParser;
 
 import java.util.Map;
 
 public class Player {
+	
+	public JsonElement json = new JsonParser().parse("{\"key1\": \"value1\", \"key2\": \"value2\"}");
 
     static final String VERSION = "Default Java folding player";
 
     public static int betRequest(JsonElement request) {
+    		
         return 200;
     }
 
