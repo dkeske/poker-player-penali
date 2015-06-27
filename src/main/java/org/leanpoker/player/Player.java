dd791ef23dd6a3ca4b37ae087c9b0381735f0d7f
Daniel Kostic <danielkeske@hotmail.com>
2015-06-27 11:50:28 +0200

    Uradili raise

--- src/main/java/org/leanpoker/player/Player.java
+++ src/main/java/org/leanpoker/player/Player.java
@@ -1,6 +1,8 @@
 package org.leanpoker.player;
 
 import com.google.gson.JsonElement;
+import com.google.gson.JsonObject;
+import com.google.gson.JsonParser;
 
 import java.util.Map;
 
@@ -9,7 +11,10 @@ public class Player {
     static final String VERSION = "We will win. Some. We will.";
 
     public static int betRequest(JsonElement request) {
-        return 200;
+    	JsonObject object = request.getAsJsonObject();
+    	String buy_in = object.get("current_buy_in").toString();
+    	
+        return Integer.parseInt(buy_in)+200;
     }
 
     public static void showdown(JsonElement game) {
