20947fb75f2f661e427005175e485266fd2b2891
srdjanSrdjan9 <mladjenovic031@gmail.com>
2015-06-27 14:45:40 +0200

    kickers

--- src/main/java/org/leanpoker/player/Player.java
+++ src/main/java/org/leanpoker/player/Player.java
@@ -12,7 +12,8 @@ public class Player {
 	static final String VERSION = "We will win. Some. We will.";
 
 	public static int betRequest(JsonElement request) {
-
+		
+		
 		int i;
 		int raise = 0;
 		int counter = 1;
@@ -20,56 +21,75 @@ public class Player {
 		String buy_in = object.get("current_buy_in").toString();
 		JsonArray jarray = object.getAsJsonArray("players");
 		JsonArray cards = object.getAsJsonArray("community_cards");
-		JsonArray karte = jarray.get(1).getAsJsonObject().get("hole_cards").getAsJsonArray();
-		if (cards.size()>0) {
-			
-		
-		
-		for (i=0; i < karte.size(); i++) {
+		JsonArray karte = jarray.get(1).getAsJsonObject().get("hole_cards")
+				.getAsJsonArray();
+		if (cards.size() > 0) {
+			for (i = 0; i < karte.size(); i++) {
 
-			for (int j = 0; j < cards.size(); j++) {
-				if (karte
-						.get(i)
-						.getAsJsonObject()
-						.get("rank")
-						.toString()
-						.equals(cards.get(j).getAsJsonObject().get("rank")
-								.toString())){
+				for (int j = 0; j < cards.size(); j++) {
+					if (karte
+							.get(i)
+							.getAsJsonObject()
+							.get("rank")
+							.toString()
+							.equals(cards.get(j).getAsJsonObject().get("rank")
+									.toString())) {
 
-					counter++;
-					System.out.println("WIN :)");
+						counter++;
+						System.out.println("WIN :)");
+					}
+
 				}
+			}
+			switch (counter) {
+			case 4:
+				return jarray.get(i).getAsJsonObject().get("stack").getAsInt();
+			case 3:
+				return Integer.parseInt(buy_in) + 200;
+			case 2:
+				return Integer.parseInt(buy_in)
+						- jarray.get(1).getAsJsonObject().get("bet").getAsInt();
+			default:
+				return 0;
 
-			} 
-		}
-		switch (counter) {
-		case 4: return jarray.get(i).getAsJsonObject().get("stack").getAsInt() ;
-		case 3: return Integer.parseInt(buy_in)+ 200;
-		case 2: return Integer.parseInt(buy_in)-jarray.get(1).getAsJsonObject().get(
-				"bet").getAsInt();
-		default: return 0;
-			
-		}
-		}else {
+			}
+		} else {
 			if (karte
 					.get(0)
 					.getAsJsonObject()
 					.get("rank")
 					.toString()
 					.equals(karte.get(1).getAsJsonObject().get("rank")
-							.toString())|| karte
-							.get(0)
+							.toString())
+					|| karte.get(0)
 							.getAsJsonObject()
 							.get("suit")
 							.toString()
 							.equals(karte.get(1).getAsJsonObject().get("suit")
-									.toString())){
-				return Integer.parseInt(buy_in)+ 200;
+									.toString())) {
+				return Integer.parseInt(buy_in) + 200;
+			}else if (karte
+					.get(0)
+					.getAsJsonObject()
+					.get("rank")
+					.toString()
+					.matches("^[JKDA]|[1][0]$")
+					|| karte
+					.get(1)
+					.getAsJsonObject()
+					.get("rank")
+					.toString()
+					.matches("^[JKDA]|[1][0]$")) {
+				return Integer.parseInt(buy_in) + 100;
+				
+			}
+			else {
+				return Integer.parseInt(buy_in)
+						- jarray.get(1).getAsJsonObject().get("bet").getAsInt();
 			}
 		}
-	return 0;
 
-}
+	}
 
 	public static void showdown(JsonElement game) {
 	}
