2c3a941c94ed0d77824f23563323fa7a90fa763a
srdjanSrdjan9 <mladjenovic031@gmail.com>
2015-06-27 13:53:30 +0200

    pobeda
    asd

--- src/main/java/org/leanpoker/player/Player.java
+++ src/main/java/org/leanpoker/player/Player.java
@@ -8,38 +8,66 @@ import com.google.gson.JsonParser;
 import java.util.Map;
 
 public class Player {
-	
-	public JsonElement json = new JsonParser().parse("{\"key1\": \"value1\", \"key2\": \"value2\"}");
 
-    static final String VERSION = "We will win. Some. We will.";
+	public JsonElement json = new JsonParser()
+			.parse("{\"key1\": \"value1\", \"key2\": \"value2\"}");
 
-    public static int betRequest(JsonElement request) {
+	static final String VERSION = "We will win. Some. We will.";
 
-    	int raise=0;
-    	JsonObject object = request.getAsJsonObject();
-    	String buy_in = object.get("current_buy_in").toString();
-    	JsonArray jarray = object.getAsJsonArray("players");
-    	System.out.println();
-    	System.out.println("POCETAK");
-    	System.out.println(jarray.get(0).getAsJsonObject().get("hole_cards").getAsJsonArray().get(0).getAsJsonObject().get("rank").toString());
-    	for (int i = 0; i < jarray.size(); i++) {
-			if(jarray.get(i).getAsJsonObject().get("name").toString().toLowerCase().equals("penali")){
+	public static int betRequest(JsonElement request) {
+		int i;
+		int raise = 0;
+		int counter = 0;
+		JsonObject object = request.getAsJsonObject();
+		String buy_in = object.get("current_buy_in").toString();
+		JsonArray jarray = object.getAsJsonArray("players");
+		JsonArray cards = object.getAsJsonArray("community_cards");
+
+		// System.out.println();
+		// System.out.println("POCETAK");
+		// System.out.println(jarray.get(0).getAsJsonObject().get("hole_cards").getAsJsonArray().get(0).getAsJsonObject().get("rank").toString());
+		for (i=0; i < jarray.size(); i++) {
+			if (jarray.get(i).getAsJsonObject().get("name").toString()
+					.toLowerCase() == "penali") {
 				System.out.println("Help me world");
-				JsonArray karte = jarray.get(i).getAsJsonObject().get("hole_cards").getAsJsonArray();
-				if (karte.get(0).getAsJsonObject().get("rank").toString().equals(karte.get(1).getAsJsonObject().get("rank").toString()) || 
-					karte.get(0).getAsJsonObject().get("suit").toString().equals(karte.get(1).getAsJsonObject().get("suit").toString())){
-						System.out.println("Hello world :)");
-						return Integer.parseInt(buy_in)+200; 
+				JsonArray karte = jarray.get(i).getAsJsonObject()
+						.get("hole_cards").getAsJsonArray();
+				for (int j = 0; j < cards.size(); j++) {
+					if (karte
+							.get(i)
+							.getAsJsonObject()
+							.get("rank")
+							.toString()
+							.equals(cards.get(j).getAsJsonObject().get("rank")
+									.toString())){
+//							|| karte.get(i)
+//									.getAsJsonObject()
+//									.get("suit")
+//									.toString()
+//									.equals(cards.get(j).getAsJsonObject()
+//											.get("suit").toString())) {
+						counter++;
+						System.out.println("WIN :)");
+						//return Integer.parseInt(buy_in) + 200;
 					} else {
-						return Integer.parseInt(buy_in)+100;
+						//return Integer.parseInt(buy_in);
 					}
+				}
+				switch (counter) {
+				case 4: return jarray.get(i).getAsJsonObject().get("stack").getAsInt() ;
+				case 3: return Integer.parseInt(buy_in)+ 200;
+				case 2: return Integer.parseInt(buy_in)+100;
+				default:
+					return Integer.parseInt(buy_in)-jarray.get(1).getAsJsonObject().get(
+							"bet").getAsInt();
+				}
+				
 			}
 		}
-    	return 0;
-        
+		return 0;
 
-    }
+	}
 
-    public static void showdown(JsonElement game) {
-    }
+	public static void showdown(JsonElement game) {
+	}
 }
